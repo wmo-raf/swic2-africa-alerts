a4e93512111f04bf64a8edd544792401c6b015d8
Erick Otenyo <otenyo.erick@gmail.com>
2023-02-27 12:21:45 +0300

    Add id to alert geojson

--- app/src/services/alertsService.js
+++ app/src/services/alertsService.js
@@ -1056,6 +1056,7 @@ const getDetail = (capLink, type) => {
     let featureColl = {
       type: "FeatureCollection",
       features: [],
+      id: alert.identifier,
     };
 
     // if is a list of areas
